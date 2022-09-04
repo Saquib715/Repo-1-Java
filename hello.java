6bef4a2397665ffa7a52732d2d705910a2065344
Cloud User <ec2-user@ip-172-31-0-222.us-west-2.compute.internal>
2022-09-04 04:58:04 +0000

    hello.java and hello.class added

---
+++ hello.java
@@ -0,0 +1,11 @@
+class hello{
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String[] args){
+     for (int i=1;i<=10;i++){
+    
+        System.out.println("Hello World ...."+i);
+
+    }
+}
+}
